9ea97cff95ca204d47e26d415b87427fce624003
Georgi Peltekov <georgi.peltekov@corp.accedia.com>
2017-06-01 21:22:41 +0300

    update version

--- main.bundle.js
+++ main.bundle.js
@@ -85,13 +85,12 @@ var AppComponent = (function () {
     function AppComponent() {
         this.title = 'app works!';
         this.files = [];
+        this.data = [];
         this.columns = [
             { title: 'Name', name: 'name' },
         ];
         this.config = {
             paging: true,
-            sorting: { columns: this.columns },
-            filtering: { filterString: '' },
             className: ['table-striped', 'table-bordered']
         };
         this.page = 1;
@@ -108,86 +107,19 @@ var AppComponent = (function () {
             var file = _a[_i];
             this.rows.push({ name: file.relativePath });
         }
+        this.data = this.rows;
         console.log(this.files);
     };
     AppComponent.prototype.onChangeTable = function (config, page) {
         if (page === void 0) { page = { page: this.page, itemsPerPage: this.itemsPerPage }; }
-        if (config.filtering) {
-            Object.assign(this.config.filtering, config.filtering);
-        }
-        if (config.sorting) {
-            Object.assign(this.config.sorting, config.sorting);
-        }
-        var filteredData = this.changeFilter(this.files, this.config);
-        var sortedData = this.changeSort(filteredData, this.config);
-        this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
-        this.length = sortedData.length;
+        // let filteredData = this.changeFilter(this.files, this.config);
+        this.rows = page && config.paging ? this.changePage(page, this.data) : this.data;
+        this.length = this.files.length;
     };
     AppComponent.prototype.changePage = function (page, data) {
-        if (data === void 0) { data = this.files; }
         var start = (page.page - 1) * page.itemsPerPage;
         var end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
         return data.slice(start, end);
-    };
-    AppComponent.prototype.changeSort = function (data, config) {
-        if (!config.sorting) {
-            return data;
-        }
-        var columns = this.config.sorting.columns || [];
-        var columnName = void 0;
-        var sort = void 0;
-        for (var i = 0; i < columns.length; i++) {
-            if (columns[i].sort !== '' && columns[i].sort !== false) {
-                columnName = columns[i].name;
-                sort = columns[i].sort;
-            }
-        }
-        if (!columnName) {
-            return data;
-        }
-        // simple sorting
-        return data.sort(function (previous, current) {
-            if (previous[columnName] > current[columnName]) {
-                return sort === 'desc' ? -1 : 1;
-            }
-            else if (previous[columnName] < current[columnName]) {
-                return sort === 'asc' ? -1 : 1;
-            }
-            return 0;
-        });
-    };
-    AppComponent.prototype.changeFilter = function (data, config) {
-        var _this = this;
-        var filteredData = data;
-        this.columns.forEach(function (column) {
-            if (column.filtering) {
-                filteredData = filteredData.filter(function (item) {
-                    return item[column.name].match(column.filtering.filterString);
-                });
-            }
-        });
-        if (!config.filtering) {
-            return filteredData;
-        }
-        if (config.filtering.columnName) {
-            return filteredData.filter(function (item) {
-                return item[config.filtering.columnName].match(_this.config.filtering.filterString);
-            });
-        }
-        var tempArray = [];
-        filteredData.forEach(function (item) {
-            var flag = false;
-            _this.columns.forEach(function (column) {
-                if (item[column.name].toString().match(_this.config.filtering.filterString)) {
-                    flag = true;
-                }
-            });
-            if (flag) {
-                tempArray.push(item);
-            }
-        });
-        filteredData = tempArray;
-        return filteredData;
     };
     return AppComponent;
 }());
