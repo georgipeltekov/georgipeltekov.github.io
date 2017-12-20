8972e240a64add1bc7a8511efc373de4ac352c50
Georgi Peltekov <georgi.peltekov@corp.accedia.com>
2017-12-20 18:29:41 +0200

    update new version

--- sources/src/app/app.module.ts
+++ sources/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
  
 import { AppComponent } from './app.component';
-import { FileDropModule } from 'ngx-file-drop/src/lib/ngx-drop';
+import { FileDropModule } from 'ngx-file-drop';
 import { Ng2TableModule } from 'ng2-table/ng2-table';
 import { PaginationModule } from 'ngx-bootstrap';
  
