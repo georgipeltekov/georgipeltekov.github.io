02c669a9b11ddf90397cea5f4032cd74c0df6ed4
Akkora <10886924+Akkora@users.noreply.github.com>
2022-01-07 13:28:40 +0100

    Update to angular 13. Maintain project

--- sources/src/app/app.component.ts
+++ sources/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import { UploadFile, UploadEvent, FileSystemFileEntry } from 'ngx-file-drop';
-
+import { Component } from '@angular/core';
+import { NgxFileDropEntry } from 'ngx-file-drop';
 
 @Component({
   selector: 'app-root',
@@ -8,26 +7,8 @@ import { UploadFile, UploadEvent, FileSystemFileEntry } from 'ngx-file-drop';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  public files: UploadFile[] = [];
-  public data: Array<any> = [];
-  public columns: Array<any> = [
-    { title: 'Name', name: 'name' },
-    { title: 'Size', name: 'size' },
-    { title: 'Last Modified Date', name: 'modified' },
-  ];
-  public config: any = {
-    paging: true,
-    className: ['table-striped', 'table-bordered']
-  };
-
-  public page: number = 1;
-  public itemsPerPage: number = 10;
-  public maxSize: number = 5;
-  public numPages: number = 1;
-  public length: number = 0;
-
-  public rows: Array<any> = [];
+  public files: NgxFileDropEntry[] = [];
+  public data: Array<{ name: string, size: string, modified: string }> = [];
 
   private units = [
     'bytes',
@@ -38,48 +19,24 @@ export class AppComponent {
     'PB'
   ];
 
-  constructor(private ref: ChangeDetectorRef) {
-  }
-
-  public dropped(event: UploadEvent) {
-    console.log("dropped")
-    this.files = event.files;
+  public dropped(files: NgxFileDropEntry[]) {
+    console.log('dropped', files)
+    this.files = files;
     this.data = [];
-    this.rows = [];
-    this.length = this.files.length;
     let index = 0;
-    for (const droppedFile of event.files) {
+    for (const droppedFile of files) {
       if (droppedFile.fileEntry.isFile) {
         const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
         fileEntry.file(fileData => {
-          // console.log(fileData);
+          console.log(fileData);
           let modifiedDate = fileData.lastModified.toLocaleString();
-          
+
           this.data.push({ name: this.files[index].relativePath, size: this.transform(fileData.size), modified: modifiedDate });
-          index++;
-          if (index == this.files.length || index == this.itemsPerPage) {
-            this.rows = this.data.slice(0, this.itemsPerPage);
-            this.length = this.files.length;
-            this.ref.markForCheck();
-            this.ref.detectChanges();
-          }
         });
       } else {
         index++;
       }
     }
-  }
-
-  public onChangeTable(config: any, page: any = { page: this.page, itemsPerPage: this.itemsPerPage }): any {
-    // let filteredData = this.changeFilter(this.files, this.config);
-    this.rows = page && config.paging ? this.changePage(page, this.data) : this.data;
-    this.length = this.files.length;
-  }
-
-  public changePage(page: any, data: Array<any>): Array<any> {
-    let start = (page.page - 1) * page.itemsPerPage;
-    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
-    return data.slice(start, end);
   }
 
   transform(bytes: number = 0, precision: number = 2): string {
@@ -94,5 +51,4 @@ export class AppComponent {
 
     return bytes.toFixed(+ precision) + ' ' + this.units[unit];
   }
-
 }
